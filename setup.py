d0fc9a6ea324a0017d1b63dacf94201ceb3b5cbb
Rob McQueen <robmcqueen@Robs-MacBook-Pro.local>
2013-09-08 15:50:49 -0700

    adding setup file for distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hypem-python',
+      version='1.0',
+      description='Python Client to Hype Machine Public API',
+      author='systemizer',
+      author_email='rob@systemizer.me',
+      url='http://github.com/systemizer/hypem-python',
+      packages=['hypem',]
+     )
